b39d620afadba1fe7d848b2457e85bc9fa8df7e1
Sudarshan Kannan <sudarshan.kannan@hiscox.com>
2018-03-19 15:46:57 -0400

    Changes made

---
+++ ContinousIntegration/src/conti/Cont.java
@@ -0,0 +1,10 @@
+package conti;
+
+public class Cont {
+
+	public static void main(String[] args) {
+	System.out.println("Continous integration is successful");
+
+	}
+
+}
--- ContinousIntegration/src/org/cont/CI.java
+++
@@ -1,9 +0,0 @@
-package org.cont;
-
-public class CI {
-	 public static void main (String args[])
-	 {
-		 System.out.println("Continous integration is started");
-	 }
-
-}
