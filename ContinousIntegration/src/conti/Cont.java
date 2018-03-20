714e4caab35124544059f5fa87511946ef37fee1
Sudarshan Kannan <sudarshan.kannan@hiscox.com>
2018-03-20 14:16:45 -0400

    added

--- ContinousIntegration/src/conti/Cont.java
+++ ContinousIntegration/src/conti/Cont.java
@@ -3,7 +3,9 @@ package conti;
 public class Cont {
 
 	public static void main(String[] args) {
-	System.out.println("Continous integration is successful");
+	
+	int a = 5000;
+	System.out.println("New Continous integration is successful "+a);
 
 	}
 
